33b267724727aa41413536aaa225e7c3c11b12b4
Jalindar Chougule <jalindar.chougule@vyom-labs.com>
2022-05-20 16:06:19 +0530

    data send

--- src/main/java/com_Utility/baseClass.java
+++ src/main/java/com_Utility/baseClass.java
@@ -12,44 +12,38 @@ import org.testng.annotations.BeforeSuite;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class baseClass {
- 
+
 	public static WebDriver driver;
 	public static configDataProvider config;
 	public static library lib;
 	public static excelDataProvider excel;
 	public static listeners list;
-	
+
 	@BeforeSuite
 	public void manage() throws IOException {
-		 config=new configDataProvider();
-		  lib=new library();
-		  excel=new excelDataProvider();
-		  list=new listeners();
+		config = new configDataProvider();
+		lib = new library();
+		excel = new excelDataProvider();
+		list = new listeners();
 	}
+
 	@BeforeMethod
 	public void openbrowser() {
 		WebDriverManager.chromedriver().setup();
-		
-		 driver=new ChromeDriver();
-		
+
+		driver = new ChromeDriver();
+
 		driver.get("http://demo.automationtesting.in/Register.html");
-		
+
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
+
 	@AfterMethod
 	public void teardown() throws InterruptedException {
-		
+
 		Thread.sleep(3000);
-		
+
 		driver.quit();
 	}
 }
